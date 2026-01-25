4dde3fd0c7565e5ea0db19e677b265e27bb0b29b
nashamri <designernasser@gmail.com>
2026-01-25 12:19:24 +0300

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module nashamri/pills
+
+go 1.25.5
---
+++ shell.nix
@@ -0,0 +1,13 @@
+let
+  # Pin nixpkgs to a specific commit
+  pkgs = import (fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/078d69f03934859a181e81ba987c2bb033eebfc5.tar.gz";
+    sha256 = "sha256:0swjr1q8d1zrnmy7mlv6jywxbqifwf9sv9bpjns0lmc0icn3y87p";
+  }) { };
+in
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    go
+  ];
+
+}
