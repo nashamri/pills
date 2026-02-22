e1196cc58ea16f6aac12d15dc4e1c1553287bff9
Saud-ALdandn <asaud6706@gmail.com>
2026-02-22 23:19:57 +0300

    restore deleted deps

--- pills/main.go
+++ pills/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"embed"
 	"fmt"
 	"time"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -140,7 +145,27 @@ type Admins struct {
 	User_Id uint
 }
 
+var assets embed.FS
+
 func main() {
+
+	app := NewApp()
+
+	// Create application with options
+	err := wails.Run(&options.App{
+		Title:  "pills",
+		Width:  1024,
+		Height: 768,
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		OnStartup:        app.startup,
+		Bind: []interface{}{
+			app,
+		},
+	})
+
 	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	if err != nil {
 		fmt.Printf("Error details: %v\n", err)
