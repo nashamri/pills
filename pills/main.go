e2473925398b3b6302805817da5190f4b4a5a844
Saud-ALdandn <asaud6706@gmail.com>
2026-02-23 22:35:51 +0300

    Create the database (closes #1)

--- pills/main.go
+++ pills/main.go
@@ -15,134 +15,119 @@ import (
 
 type User struct {
 	gorm.Model
-	Id         uint
-	F_Name     string
-	L_Name     string
-	UserName   string
-	Email      string
-	Password   string
-	Gender     string
-	Role       UserRole
-	Created_at time.Time
+	FirstName string
+	LastName  string
+	UserName  string
+	Email     string
+	Password  string
+	Gender    string
+	Role      UserRole
 }
 
-type UserRole string
+type UserRole int
 
 const (
-	Admin      UserRole = "Admin"
-	Patients   UserRole = "patients"
-	Caregivers UserRole = "Caregivers"
+	Admin = iota
+	Patients
+	Caregivers
 )
 
 type Schedules struct {
 	gorm.Model
-	Id             uint
-	Patient_id     uint
-	Pedication_id  uint
-	Start_date     time.Time
-	End_date       time.Time
-	Start_hour     time.Time
-	Interval_hours uint
-	Instructions   string
-	Dosage         string
-	Quantity       uint
+	MedicationId  uint
+	StartDate     time.Time
+	EndDate       time.Time
+	StartHour     time.Time
+	IntervalHours uint
+	Instructions  string
+	Dosage        string
+	Quantity      uint
 }
 
 type patients struct {
 	gorm.Model
-	User_Id       uint
-	date_of_birth time.Time
-	medical_notes string
+	DateOfBirth  time.Time
+	MedicalNotes string
 }
 type caregivers struct {
 	gorm.Model
-	User_Id     uint
-	Id          uint
-	patients_id uint
+	UserId     uint
+	PatientsId uint
 }
 type Measurements struct {
 	gorm.Model
-	Id           uint
-	Patient_id   uint
-	caergiver_id uint
-	Type         UserRole
-	value        string
-	measured_at  time.Time
-	notes        string
+	PatientId   uint
+	CaregiverId uint
+	Type        UserRole
+	Value       string
+	MeasuredAt  time.Time
+	Notes       string
 }
-type report struct {
+type Report struct {
 	gorm.Model
-	Id           uint
-	Patient_id   uint
-	Caregiver_id uint
-	file_path    string
-	generated_at time.Time
+	PatientId   uint
+	CaregiverId uint
+	FilePath    string
 }
 type Notifications struct {
 	gorm.Model
-	Id                 uint
-	Patient_id         uint
-	Medication_id      uint
-	Notification_sound string
-	Type               NotificationType
-	Sent_at            time.Time
-	Read               bool
+	PatientId         uint
+	MedicationId      uint
+	NotificationSound string
+	Type              NotificationType
+	SentAt            time.Time
+	Read              bool
 }
 
-type NotificationType string
+type NotificationType int
 
 const (
-	Voice NotificationType = "Voice"
-	Text  NotificationType = "Text"
+	Voice NotificationType = iota
+	Text
 )
 
 type Preferences struct {
 	gorm.Model
-	Id            uint
-	User_Id       uint
-	Language      string
-	Font_Size     string
-	Contrast_Mode string
-	Theme         string
+	UserId       uint
+	Language     string
+	FontSize     string
+	ContrastMode string
+	Theme        string
 }
 
 type Drug_Database struct {
 	gorm.Model
-	Id           uint
 	Name         string
 	Description  string
 	Interactions string
 	Manufacturer string
-	Image_url    string
+	ImageUrl     string
 }
 type Logs struct {
 	gorm.Model
-	Id          uint
-	Schedule_Id uint
-	Taken_At    time.Time
-	Note        string
+	ScheduleId uint
+	TakenAt    time.Time
+	Note       string
 }
 
 type Medications struct {
 	gorm.Model
-	Id           uint
-	Drug_Info_Id uint
-	Name         string
-	Photo_url    string
-	Type         MedicationType
+	DrugInfoId uint
+	Name       string
+	PhotoUrl   string
+	Type       MedicationType
 }
 type MedicationType string
 
 const (
-	Dose  MedicationType = "Dose"
-	Pill  MedicationType = "Pill"
-	Drink MedicationType = "Drink"
+	Dose = iota
+	Pill
+	Drink
 )
 
 type Admins struct {
 	gorm.Model
-	Id      uint
-	User_Id uint
+	UserId uint
 }
 
 var assets embed.FS
@@ -174,8 +159,8 @@ func main() {
 
 	db.AutoMigrate(&User{})
 
-	db.Create(&User{Id: 1, F_Name: "saud", L_Name: "ahmad", UserName: "saud111", Email: "saud@.com", Password: "password123", Gender: "Male", Role: Admin, Created_at: time.Now()})
-	db.Create(&User{Id: 2, F_Name: "fahad", L_Name: "farhan", UserName: "fahad111", Email: "fahad@.com", Password: "password123", Gender: "Male", Role: Admin, Created_at: time.Now()})
-	db.Create(&User{Id: 3, F_Name: "ahmad", L_Name: "nasser", UserName: "ahmad111", Email: "ahmad@.com", Password: "password123", Gender: "Male", Role: Admin, Created_at: time.Now()})
+	db.Create(&User{FirstName: "saud", LastName: "ahmad", UserName: "saud111", Email: "saud@.com", Password: "password123", Gender: "Male", Role: Admin})
+	db.Create(&User{FirstName: "fahad", LastName: "farhan", UserName: "fahad111", Email: "fahad@.com", Password: "password123", Gender: "Male", Role: Admin})
+	db.Create(&User{FirstName: "ahmad", LastName: "nasser", UserName: "ahmad111", Email: "ahmad@.com", Password: "password123", Gender: "Male", Role: Admin})
 
 }
